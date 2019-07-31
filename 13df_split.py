e7fa097f648956d46715f73a6ef70f74360db58f
Reid00 <isreid.zhang@outlook.com>
2019-07-31 18:37:38 +0800

    add pandas str.split another use scenairo

--- 13df_split.py
+++ 13df_split.py
@@ -4,9 +4,10 @@ students = pd.read_excel('students.xlsx', sheet_name='students')
 
 # Series 用str 转化为str field
 df = students['Name'].str.split('_', expand=True)
-# print(df[0])
-
+# # print(df[0])
 students['common_name'] = df[0]
 students['id_name'] = df[1]
+
+# students['common_name'], students['id_name'] = students['Name'].str.split('_').str  # 另外一种方法
 
 print(students)
